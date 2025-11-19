5ea18af1a9adba668ab5f0c07a30f4bab21c2c3d
1t1sCooL <malabugin@anykey.pro>
2025-11-19 17:01:11 +0300

    fix my frontend-mentor url

--- src/shared/ui/footer/ui/Footer.tsx
+++ src/shared/ui/footer/ui/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <footer>
             <p className={styles.footer}>
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"> Frontend Mentor</a>
-                Coded by <a href="#">1t1sCooL</a>
+                Coded by <a href="https://www.frontendmentor.io/profile/1t1sCooL">1t1sCooL</a>
             </p>
         </footer>
     )
