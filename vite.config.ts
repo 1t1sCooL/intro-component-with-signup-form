d1e53e344c0fc11d946c685b8e3b325a672a9833
1t1sCooL <mmalabugin@gmail.com>
2026-01-17 17:41:21 +0300

    add more interactivity

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from "node:path";
+import react from '@vitejs/plugin-react';
+import * as path from 'node:path';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const base = env.VITE_BASE_PATH ?? '/';
+
+  return {
+    base,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  //base: '/IntroComponentWithSignupForm/',
-})
+  };
+});
