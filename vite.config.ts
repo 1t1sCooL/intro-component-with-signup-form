7d0e5ffd59007d45f2beb5312f5316dec09f9cd1
1t1sCooL <mmalabugin@gmail.com>
2026-01-17 18:36:05 +0300

    add nginx file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const base = env.VITE_BASE_PATH ?? '/';
+  const base = process.env.VITE_BASE_PATH ?? env.VITE_BASE_PATH ?? '/';
 
   return {
     base,
