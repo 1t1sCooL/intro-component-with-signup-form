75671cb0c38293ede18e24a8dd8eb771ba44a138
1t1sCooL <mmalabugin@gmail.com>
2026-01-17 19:00:08 +0300

    fix!

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
 import react from '@vitejs/plugin-react';
 import * as path from 'node:path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const base = process.env.VITE_BASE_PATH ?? env.VITE_BASE_PATH ?? '/';
-
-  return {
-    base,
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-  };
+  },
+  base: '/IntroComponentWithSignupForm/',
 });
